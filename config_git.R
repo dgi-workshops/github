3fdb00b14490a98b007b4745c9d9131d44f3c084
dgi-workshops <105821195+dgi-workshops@users.noreply.github.com>
2022-08-01 21:04:45 -0300

    Update config_git.R

--- config_git.R
+++ config_git.R
@@ -25,7 +25,7 @@ usethis::create_github_token()
 ## in case usethis version < 2.0.0: usethis::browse_github_token() (or even better: update usethis!)
 
 ## set personal access token:
-credentials::set_github_pat("YourPAT")
+credentials::set_github_pat()
 
 ## or store it manually in '.Renviron':
 usethis::edit_r_environ()
